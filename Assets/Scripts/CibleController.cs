30d7f7c16f88cece82a9ffc5f3cceceffa4fcbd2
RomainReina <romain.reina98@gmail.com>
2021-01-31 18:14:17 +0100

    Add Jeu Cible
    pas fini faut mettre le gun !

---
+++ Assets/Scripts/CibleController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CibleController : MonoBehaviour
+{
+    [SerializeField] private Transform spawnPointTransform;
+    [SerializeField] private Transform CibleHolderTransform;
+
+
+    
+
+    
+    // Update is called once per frame
+    void Start()
+    {
+        
+            Vector3 posGO = spawnPointTransform.position;
+            for (int i = 0; i < 5; i++)
+            {
+                Vector3 newPos = posGO + new Vector3(0f,0f, i*3);
+            
+                Instantiate(Resources.Load("Cible"),
+                    newPos,
+                    Quaternion.identity,
+                    CibleHolderTransform);
+            }
+            
+
+            
+    }
+
+    
+
+    
+}
+           
+
